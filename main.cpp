2315d381d55d55ca1d410cf73729caa8bd3ffcf8
MrJohnRobert <1756590334@qq.com>
2018-10-31 19:26:42 +0800

    无法提交时错误解决办法

--- main.cpp
+++ main.cpp
@@ -7,6 +7,17 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     //此为第一次测试
+//    *** Please tell me who you are.
+
+//    Run
+
+//      git config --global user.email "you@example.com"
+//      git config --global user.name "Your Name"
+
+//    to set your account's default identity.
+//    Omit --global to set the identity only in this repository.
+  //  解决办法 https://blog.csdn.net/coco_wonderful/article/details/51822143
+
 
     return a.exec();
 }
